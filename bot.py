f804939b7b02619ebbecb912f1ec4b84639d8a95
ali9313 <lioali65808@gmail.com>
2024-10-12 21:49:43 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -42,6 +42,31 @@ def load_data_from_json():
     cursor.close()
     conn.close()
 
+# دالة لإضافة المستخدم
+def add_user(user_id, username):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO users (user_id, username) VALUES (%s, %s) ON CONFLICT (user_id) DO NOTHING",
+        (user_id, username)
+    )
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+# دالة للحصول على النقاط
+def get_points(user_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT points FROM users WHERE user_id = %s", (user_id,))
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    
+    if result:
+        return result[0]  # إرجاع النقاط إذا تم العثور عليها
+    return 0  # إرجاع 0 إذا لم يتم العثور على المستخدم
+
 # الدوال الخاصة بالبوت
 @bot.message_handler(commands=['start'])
 def start_message(message):
